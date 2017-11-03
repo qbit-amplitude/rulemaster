8160ab8d68cd87d0714f9dd6eb6a6ca0d6625b56
Aditya Kalyanakrishnan <akalyana@akamai.com>
2017-11-04 05:04:10 +0530

    url bug fixes

--- engine/urls.py
+++ engine/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^v1/health$', views.health, name='health'),
-    url(r'^v1/?P<ruleset_name>[_0-9a-ZA-Z\-]+/definition', views.ruleset_definition, name="ruleset_definition")
-    url(r'^v1/?P<ruleset_name>[_0-9a-ZA-Z\-]+/facts', views.ruleset_facts, name="ruleset_facts")
-    url(r'^v1/?P<ruleset_name>[_0-9a-ZA-Z\-]+/events', views.ruleset_events, name="ruleset_events")
+    url(r'^v1/(?P<ruleset_name>[_0-9a-zA-Z\-]+)/definition', views.ruleset_definition, name="ruleset_definition"),
+    url(r'^v1/(?P<ruleset_name>[_0-9a-zA-Z\-]+)/facts', views.ruleset_facts, name="ruleset_facts"),
+    url(r'^v1/(?P<ruleset_name>[_0-9a-zA-Z\-]+)/events', views.ruleset_events, name="ruleset_events"),
 ]
