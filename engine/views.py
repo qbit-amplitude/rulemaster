1a2e7834ebd92884bb086e749a8a470f303fca44
Aditya Kalyanakrishnan <akalyana@akamai.com>
2017-11-03 21:53:17 +0530

    added basic health url

--- engine/views.py
+++ engine/views.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+import datetime
+from django.http import JsonResponse
 
 # Create your views here.
+def health(request):
+    now = datetime.datetime.now().isoformat()
+    resp = {
+        "alive": True,
+        "timestamp": now
+    }
+    return JsonResponse(resp)
